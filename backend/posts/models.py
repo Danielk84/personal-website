6222ddb8228c4c35c0aed2a205abcab3059f9468
DanielK84 <daniel-k84@outlook.com>
2025-04-12 14:21:46 +0330

    Add on backend: base urls.py file and base Post model in posts app

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.utils import timezone
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class PostQuerySet(models.QuerySet):
+    """
+    Custom QuerySet for filtering published and active posts.
+
+    This QuerySet provides convenient methods for retrieving posts
+    based on their publication status and activity.
+    """
+
+    def published(self) -> models.QuerySet:
+        """
+        Returns only published posts.
+
+        A post is considered published if:
+        - It is active.
+        - Its `pub_date` is in the past (before or equal to the current time).
+
+        Returns:
+            QuerySet: A queryset containing published posts.
+        """
+        return self.active().filter(pub_date__lte=timezone.now())
+
+    def active(self) -> models.QuerySet:
+        """
+        Returns only active posts.
+
+        A post is considered active if its `is_active` field is set to `True`.
+
+        Returns:
+            QuerySet: A queryset containing active posts.
+        """
+        return self.filter(is_active=True)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(allow_unicode=True, unique=True)
+    is_active = models.BooleanField(default=False)
+    body = models.TextField()
+    summary = models.TextField()
+    pub_date = models.DateTimeField(default=timezone.now)
+    last_modify = models.DateTimeField(auto_now=True)
+
+    objects = PostQuerySet.as_manager()
+
+    class Meta:
+        ordering = ["-pub_date"]
+        indexes = [
+            models.Index(fields=["title", "is_active", "pub_date", "last_modify"])
+        ]
+
+    def __str__(self) -> str:
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(f"{self.title}-{self.pub_date}")
+        super().save(*args, **kwargs)
+
+    def is_published(self) -> bool:
+        return self.is_active and self.pub_date < timezone.now()
--- backend/posts/tests.py
+++ backend/posts/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from django.template.defaultfilters import slugify
 
-# Create your tests here.
+from .models import Post
+
+class PostTestCase(TestCase):
+    def setUp(self):
+        for i in range(5):
+            Post.objects.create(
+                title=f"title: {i}",
+                is_active=True if (i & 1) == 0 else False,
+                body=f"body {i}",
+                summary=f"summary {i}",
+            )
+
+    def test_active_queryset(self):
+        posts = Post.objects.active()
+        self.assertEqual(posts.count(), 3)
+
+    def test_published_queryset(self):
+        posts = Post.objects.published()
+        self.assertEqual(posts.count(), 3)
+
+    def test_slug_value(self):
+        post = Post.objects.first()
+        self.assertEqual(post.title, "title: 4")
+
+        self.assertEqual(
+            post.slug,
+            slugify(f"{post.title}-{post.pub_date}"),
+        )
+
+    def test_save_method(self):
+        post = Post.objects.last()
+        self.assertEqual(post.title, "title: 0")
+
+        post.title = "changing post title"
+        last_modify = post.last_modify
+        post.save()
+        self.assertNotEqual(post.last_modify, last_modify)
