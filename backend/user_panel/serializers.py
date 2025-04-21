bf51962324c724e13acf76d8665c0519072fcc00
Daniel-k84 <daniel-k84@outlook.com>
2025-04-21 15:21:26 +0330

    Add on backend: base AdminManagerMixin and Post and Photo Activation ViewSets with serializer creators

--- backend/user_panel/serializers.py
+++ backend/user_panel/serializers.py
@@ -4,3 +4,21 @@ from rest_framework import serializers
 class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=64)
     password = serializers.CharField(max_length=32)
+
+
+def create_activation_serializer(base_model):
+    class BaseActivationSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = base_model
+            fields = ["title", "is_active", "slug"]
+
+    return BaseActivationSerializer
+
+
+def create_full_serializer(base_model):
+    class BaseFullSerialzier(serializers.ModelSerializer):
+        class Meta:
+            model = base_model
+            fields = "__all__"
+
+    return BaseFullSerialzier
--- backend/user_panel/views.py
+++ backend/user_panel/views.py
@@ -1,13 +1,19 @@
 from http import HTTPMethod
 
 from django.contrib.auth import authenticate
-from rest_framework import status
+from rest_framework import status, viewsets, permissions, mixins
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from .auth import generate_token
-from .serializers import UserLoginSerializer
+from .auth import generate_token, OnlyAdminJWTAuthetication
+from .serializers import (
+    UserLoginSerializer,
+    create_activation_serializer,
+    create_full_serializer,
+)
+from posts.models import Post
+from medias.models import Photo
 
 
 @api_view([HTTPMethod.POST])
@@ -26,3 +32,33 @@ def jwt_login(req: Request):
         return Response({"Token": token})
     except Exception:
         return Response(status=status.HTTP_403_FORBIDDEN)
+
+
+class AdminManagerMixin(
+    mixins.ListModelMixin,
+    mixins.UpdateModelMixin,
+):
+    queryset = ...
+    lookup_field = "slug"
+    authentication_classes = [OnlyAdminJWTAuthetication]
+    permission_classes = [permissions.IsAuthenticated]
+    full_serializer = ...
+
+    def retrieve(self, slug: str):
+        try:
+            obj = self.queryset.get(slug=slug)
+            return Response(self.full_serializer(obj).data)
+        except Exception:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class PostActicationViewSet(AdminManagerMixin, viewsets.GenericViewSet):
+    queryset = Post.objects.order_by("is_active")
+    serializer_class = create_activation_serializer(Post)
+    full_serializer = create_full_serializer(Post)
+
+
+class PhototActivationViewSet(AdminManagerMixin, viewsets.GenericViewSet):
+    queryset = Photo.objects.order_by("is_active")
+    serializer_class = create_activation_serializer(Photo)
+    full_serializer = create_full_serializer(Photo)
