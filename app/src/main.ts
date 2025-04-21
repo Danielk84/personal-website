071656393f1421ff92ffaca8f490a31bb7c36e65
Daniel-k84 <daniel-k84@outlook.com>
2025-04-21 18:19:21 +0330

    Fix bug on app: dynamic import for components.

--- app/src/main.ts
+++ app/src/main.ts
@@ -5,31 +5,21 @@ import { createApp, defineAsyncComponent } from 'vue'
 import App from './App.vue'
 import router from "./router"
 
-const app = createApp(App)
-  .component("HeaderLayer", defineAsyncComponent(() => 
-    import("./components/HeaderLayer.vue")
-  ))
-  .component("PagesBar", defineAsyncComponent(() =>
-    import("./components/PagesBar.vue")
-  ))
-  .component("FirstButton", defineAsyncComponent(() =>
-    import("./components/FirstButton.vue")
-  ))
-  .component("SecondButton", defineAsyncComponent(() => 
-    import("./components/SecondButton.vue")
-  ))
-  .component("PostBox", defineAsyncComponent(() =>
-    import("./components/PostBox.vue")
-  ))
-  .component("AsideLayer", defineAsyncComponent(() => 
-    import("./components/AsideLayer.vue")
-  ))
-  .component("PostOverview", defineAsyncComponent(() =>
-    import("./components/PostOverview.vue")
-  ))
-  .component("FooterLayer", defineAsyncComponent(() => 
-    import("./components/FooterLayer.vue")
-  ))
-.use(router)
+const component = [
+  "HeaderLayer", 
+  "PagesBar",
+  "FirstButton",
+  "SecondButton",
+  "PostBox",
+  "AsideLayer",
+  "PostOverview",
+  "FooterLayer",
+]
+
+const app = createApp(App).use(router);
+
+component.forEach(name => 
+  app.component(name, defineAsyncComponent( () => import(`./components/${name}.vue`)))
+);
 
 app.mount('#app');
