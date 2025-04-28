c1d5c90f791bd0794c3bf7b45ff1c8237fbcbf23
Daniel-k84 <daniel-k84@outlook.com>
2025-04-28 19:15:01 +0330

    Add on app: base content on PostOverview component.

--- app/src/components/PostOverview.vue
+++ app/src/components/PostOverview.vue
@@ -1,11 +1,22 @@
+<script setup lang="ts">
+import type { PostOverview } from '../interfaces/PostSerializers';
+
+defineProps<PostOverview>();
+</script>
+
 <template>
   <div class="flex justify-center">
-    <RouterLink to="/post">
+    <RouterLink :to="`/post/${slug}/`" class="max-md:w-8/10 min-md:w-6/10">
       <div class="ref-box-color text-f-2 border-btn-1 m-4 px-4 py-8 rounded-xl
-        min-h-[200px] min-w-[300px] max-xl:max-w-2xl min-xl:max-w-4xl
-        delay-75 duration-500 hover:scale-105 hover:drop-shadow-2xl hover:border-4">
-        <h2>hello</h2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam qui nulla vitae voluptatibus minus harum, ab illum molestias, sequi, atque commodi amet rerum debitis culpa illo. Atque aspernatur ex laudantium!</p>
+        min-h-[200px] delay-75 duration-150
+        hover:scale-105 hover:drop-shadow-2xl hover:border-4">
+      <h2>{{ title }}</h2>
+      <p>{{ summary }}</p>
+      <hr class="border-btn-1 border-2 my-4"/>
+      <div class="flex flex-col items-center">
+        <time>{{ pub_date }}</time>
+        <p>{{ user }}</p>
+      </div>
       </div>
     </RouterLink>
   </div>
--- app/src/pages/Posts.vue
+++ app/src/pages/Posts.vue
@@ -1,15 +1,33 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import type { PostOverview } from '../interfaces/PostSerializers';
+import type BasePagination from '../interfaces/BasePagination';
+import { getStaticData } from '../composables/fetchData';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const contents = ref<PostOverview[]>();
+
+onMounted(async () => {
+  try {
+    const { json, statusCode, msg } = await getStaticData("/posts/");
+    if (statusCode !== 200) throw msg;
+
+    contents.value = (json as BasePagination<PostOverview>).results;
+  } catch (error) {
+    router.push(`/404/${error}`);
+  }
+});
+</script>
+
 <template>
-  <div class="grid grid-cols-1">
-    <PostOverview />
-    <PostOverview />
-    <PostOverview />
-    <PostOverview />
-    <PostOverview />
-    <PostOverview />
-    <PostOverview />
-    <PostOverview />
-    <PostOverview />
-    <PostOverview />
+  <div v-if="contents">
+    <div class="grid grid-cols-1 w-screen">
+      <div v-for="content in contents">
+        <PostOverview :title="content.title" :slug="content.slug" :user="content.user"
+          :summary="content.summary" :pub_date="content.pub_date" />
+      </div>
+    </div>
   </div>
   <div>
     <PaginationManager />
